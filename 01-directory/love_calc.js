e46ab1486461ea98c009cbdb36304c0610a8eb92
Esseh12 <gbenmoese788@gmail.com>
2023-12-04 12:29:43 +0100

    git commit -m "all"

---
+++ 01-directory/love_calc.js
@@ -0,0 +1,6 @@
+function getInputValue(){
+  var name1 = document.querySelectorAll('.name')[0].value;
+  var name2 = document.querySelectorAll('.name')[1].value;
+  var name = Math.random();
+  name = name * 100;
+  name = Math.floor(name) + 1;
