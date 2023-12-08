01d9c1a41dbf3e6d24823af0989a820dd2985601
Esseh12 <gbenmoese788@gmail.com>
2023-12-08 21:56:06 +0100

    git commit -m "still working on love calculator"

--- 01-directory/love_calc.js
+++ 01-directory/love_calc.js
@@ -1,6 +1,20 @@
 function getInputValue(){
   var name1 = document.querySelectorAll('.name')[0].value;
   var name2 = document.querySelectorAll('.name')[1].value;
+ 
   var name = Math.random();
   name = name * 100;
   name = Math.floor(name) + 1;
+   
+  if (name === 1 && name < 30){
+    document.querySelector("img").src = ""
+  }
+
+  else if (name === 31 && name < 60){
+    document.querySelector("img").src = "./white_dance.jpg"
+  }
+
+  else {
+    alert("greter");
+  }
+}
